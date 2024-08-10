a54c9e0bb7d6988bee200c33398b6730f8fe008a
Abdulwaisa Ayed Naji Al Nuaimi <alnymybdalwas1@gmail.com>
2024-08-11 03:13:00 +1300

    Add project files.

---
+++ Data/BankSystem/BankSystem.Data/BankSystemDbContext.cs
@@ -0,0 +1,24 @@
+﻿namespace BankSystem.Data
+{
+    using Configurations;
+    using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+    using Microsoft.EntityFrameworkCore;
+    using Models;
+
+    public class BankSystemDbContext(DbContextOptions<BankSystemDbContext> options)
+        : IdentityDbContext<BankUser>(options)
+    {
+        public DbSet<BankAccount> Accounts { get; set; }
+
+        public DbSet<MoneyTransfer> Transfers { get; set; }
+
+        public DbSet<Card> Cards { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.ApplyConfiguration(new UserConfiguration());
+
+            base.OnModelCreating(builder);
+        }
+    }
+}
---
+++ DemoShop/DemoShop.Web/Startup.cs
@@ -0,0 +1,91 @@
+﻿namespace DemoShop.Web
+{
+    using Configuration;
+    using Data;
+    using DemoShop.Models;
+    using Extensions;
+    using Microsoft.AspNetCore.Builder;
+    using Microsoft.AspNetCore.Hosting;
+    using Microsoft.AspNetCore.Identity;
+    using Microsoft.AspNetCore.Mvc;
+    using Microsoft.EntityFrameworkCore;
+    using Microsoft.Extensions.Configuration;
+    using Microsoft.Extensions.DependencyInjection;
+    using Microsoft.Extensions.Hosting;
+    using Services.Implementations;
+    using Services.Interfaces;
+
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+            => this.Configuration = configuration;
+
+        private IConfiguration Configuration { get; }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddDbContext<DemoShopDbContext>(options =>
+                options.UseSqlServer(this.Configuration.GetConnectionString("DefaultConnection")));
+
+            services.AddIdentity<DemoShopUser, IdentityRole>(options =>
+                {
+                    options.Password.RequiredLength = 6;
+                    options.Password.RequireDigit = false;
+                    options.Password.RequireLowercase = false;
+                    options.Password.RequireUppercase = false;
+                    options.Password.RequireNonAlphanumeric = false;
+                    options.Password.RequiredUniqueChars = 0;
+                })
+                .AddDefaultUI()
+                .AddEntityFrameworkStores<DemoShopDbContext>()
+                .AddDefaultTokenProviders();
+
+            services.ConfigureApplicationCookie(options => { options.Cookie.Name = "DemoShopLogin"; });
+
+            services
+                .Configure<DestinationBankAccountConfiguration>(
+                    this.Configuration.GetSection(nameof(DestinationBankAccountConfiguration)))
+                .Configure<DirectPaymentsConfiguration>(
+                    this.Configuration.GetSection(nameof(DirectPaymentsConfiguration)))
+                .Configure<CardPaymentsConfiguration>(
+                    this.Configuration.GetSection(nameof(CardPaymentsConfiguration)));
+
+            services.AddControllersWithViews();
+            services.AddRazorPages();
+
+            services.AddScoped<IProductsService, ProductsService>();
+            services.AddScoped<IOrdersService, OrdersService>();
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            app.InitializeDatabaseAsync().GetAwaiter().GetResult();
+
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseDatabaseErrorPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+            }
+
+            app.UseRouting();
+            app.UseStatusCodePages();
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+            app.UseAuthentication();
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllerRoute(
+                    "areas",
+                    "{area:exists}/{controller=Home}/{action=Index}/{id?}");
+                endpoints.MapDefaultControllerRoute();
+                endpoints.MapRazorPages();
+            });
+        }
+    }
+}
